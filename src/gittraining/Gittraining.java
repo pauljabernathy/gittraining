e1129d9cd27fd5b2276b7696fc146cf0c132bce9
pauljabernathy <pauljabernathy@gmail.com>
2014-09-21 16:24:02 -0400

    first commit

---
+++ src/gittraining/Gittraining.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittraining;
+
+/**
+ *
+ * @author paul
+ */
+public class Gittraining {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("for practicing with git and github, so I make mistakes here and not on actual code repositories that I don't want to mess up");
+        System.out.println("master branch");
+    }
+}
