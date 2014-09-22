e1a59a5b8fcddb8f41d019d2119e99ba32192ac6
pauljabernathy <pauljabernathy@gmail.com>
2014-09-22 10:01:47 -0400

    adding IB1_1.java

---
+++ src/gittraining/IB1_1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gittraining;
+
+/**
+ *
+ * @author paul
+ */
+public class IB1_1 {
+    public String getMessage() {
+        return "first class made on the 1b1 branch";
+    }
+}
