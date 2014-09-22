7e763082e7dffeda2526e4a7fb218b64335f7418
pauljabernathy <pauljabernathy@gmail.com>
2014-09-21 22:11:31 -0400

    sunday night commit

--- src/gittraining/Gittraining.java
+++ src/gittraining/Gittraining.java
@@ -17,5 +17,6 @@ public class Gittraining {
         System.out.println("for practicing with git and github, so I make mistakes here and not on actual code repositories that I don't want to mess up");
         System.out.println("master branch");
         //another change
+        //yet another change
     }
 }
